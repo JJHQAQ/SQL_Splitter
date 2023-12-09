99a8aa22a6ebe294524ff106234f08f47a7984ab
KKKQQQHHH <23125317@bjtu.edu.cn>
2023-12-09 22:06:37 +0800

    supplementary level query

--- dbmanager/select.go
+++ dbmanager/select.go
@@ -28,19 +28,46 @@ func (dbmp *DBM) Select(sql_s string) {
 		return
 	}
 
-	sql_no_join := util.Join_fileter(sql_s, dbmp.tables)
+	var sql_no_join string
+
+	// Check if the SQL statement contains a WHERE clause
+	if tree.Where != nil && tree.Where.Expr != nil {
+		sql_no_join = util.Join_fileter(sql_s, dbmp.tables)
+	} else {
+		sql_no_join = sql_s
+	}
 
 	fmt.Println(sql_no_join)
 	for i, table := range tree.From {
 		table_name := sqlparser.GetTableName(table.(*sqlparser.AliasedTableExpr).Expr).String() //获取表名
 
-		table_sql := util.Table_filter(sql_no_join, i, dbmp.tables[table_name].Columns)
-		fmt.Println(dbmp.tables[table_name].Columns)
-		fmt.Println(table_sql)
+		var table_sql string
+		if tree.Where != nil && tree.Where.Expr != nil {
+			table_sql = util.Table_filter(sql_no_join, i, dbmp.tables[table_name].Columns)
+		} else {
+			table_sql = sql_s
+		}
+
+		// fmt.Println(dbmp.tables[table_name].Columns)
+		// fmt.Println(table_sql)
 
 		if dbmp.tables[table_name].Mode == "h" { //水平分片
-			items := dbmp.horizontal_fragmentation(table_sql, table_name)
-			result[table_name] = items
+			items, rowCount, colCount, siteNames := dbmp.horizontal_fragmentation(table_sql, table_name)
+			result[table_name] = map[string]interface{}{
+				"items":     items,
+				"rowCount":  rowCount,
+				"colCount":  colCount,
+				"siteNames": siteNames,
+			}
+			for tableName, info := range result {
+				fmt.Printf("表名：%s\n", tableName)
+				fmt.Printf("行数：%d\n", info.(map[string]interface{})["rowCount"].(int))
+				fmt.Printf("列数：%d\n", info.(map[string]interface{})["colCount"].(int))
+				fmt.Printf("站点：%v\n", info.(map[string]interface{})["siteNames"].([]string))
+				fmt.Println("查询结果:")
+				fmt.Println(info.(map[string]interface{})["items"])
+				fmt.Println("---------------")
+			}
 			// PrintAll(items)
 		}
 		if dbmp.tables[table_name].Mode == "v" { //垂直分片
@@ -55,18 +82,30 @@ func (dbmp *DBM) Select(sql_s string) {
 
 // TODO 加更多的表类型，以及改成泛型func
 // Customer
-func (dbmp *DBM) horizontal_fragmentation(sql_s string, TableName string) interface{} {
+func (dbmp *DBM) horizontal_fragmentation(sql_s string, TableName string) ([]string, int, int, []string) {
 	//customer
 	if TableName == "customer" {
 		var customers []datatype.Customer
+		rowCount := 0
+		colCount := 0
+		var siteNames []string
+
 		for _, site := range dbmp.tables[TableName].Sites {
 			rows, e := dbmp.Databases[site].Query(sql_s)
 			if e != nil {
 				// do something
 				fmt.Println(e)
-				return nil
+				return nil, 0, 0, nil
 			}
+			// 获取列名
+			columns, _ := rows.Columns()
+			colCount = len(columns)
+
+			hasResult := false
+
 			for rows.Next() {
+				hasResult = true
+				rowCount++
 				var customer datatype.Customer
 				e := rows.Scan(&customer.Id, &customer.Name, &customer.Gender)
 				if e == nil {
@@ -76,19 +115,40 @@ func (dbmp *DBM) horizontal_fragmentation(sql_s string, TableName string) interf
 				}
 			}
 			rows.Close()
+			// 只有在站点有结果时才加入站点列表
+			if hasResult {
+				siteNames = append(siteNames, site)
+			}
 		}
-		return customers
+		// 将customers转换为字符串切片
+		var result []string
+		for _, customer := range customers {
+			result = append(result, fmt.Sprintf("%+v", customer))
+		}
+		return result, rowCount, colCount, siteNames
 	}
 	//orders
 	if TableName == "orders" {
 		var orders []datatype.Orders
+		rowCount := 0
+		colCount := 0
+		var siteNames []string
+
 		for _, site := range dbmp.tables[TableName].Sites {
 			rows, e := dbmp.Databases[site].Query(sql_s)
 			if e != nil {
 				fmt.Println(e)
-				return nil
+				return nil, 0, 0, nil
 			}
+			// 获取列名
+			columns, _ := rows.Columns()
+			colCount = len(columns)
+
+			hasResult := false
+
 			for rows.Next() {
+				hasResult = true
+				rowCount++
 				var order datatype.Orders
 				e := rows.Scan(&order.Customer_id, &order.Book_id, &order.Quantity)
 				if e == nil {
@@ -98,19 +158,40 @@ func (dbmp *DBM) horizontal_fragmentation(sql_s string, TableName string) interf
 				}
 			}
 			rows.Close()
+			// 只有在站点有结果时才加入站点列表
+			if hasResult {
+				siteNames = append(siteNames, site)
+			}
 		}
-		return orders
+		// 将orders转换为字符串切片
+		var result []string
+		for _, order := range orders {
+			result = append(result, fmt.Sprintf("%+v", order))
+		}
+		return result, rowCount, colCount, siteNames
 	}
 	//publishers
 	if TableName == "publishers" {
 		var publishers []datatype.Publishers
+		rowCount := 0
+		colCount := 0
+		var siteNames []string
+
 		for _, site := range dbmp.tables[TableName].Sites {
 			rows, e := dbmp.Databases[site].Query(sql_s)
 			if e != nil {
 				fmt.Println(e)
-				return nil
+				return nil, 0, 0, nil
 			}
+			// 获取列名
+			columns, _ := rows.Columns()
+			colCount = len(columns)
+
+			hasResult := false
+
 			for rows.Next() {
+				hasResult = true
+				rowCount++
 				var publisher datatype.Publishers
 				e := rows.Scan(&publisher.Id, &publisher.Name, &publisher.State)
 				if e == nil {
@@ -120,10 +201,19 @@ func (dbmp *DBM) horizontal_fragmentation(sql_s string, TableName string) interf
 				}
 			}
 			rows.Close()
+			// 只有在站点有结果时才加入站点列表
+			if hasResult {
+				siteNames = append(siteNames, site)
+			}
 		}
-		return publishers
+		// 将publishers转换为字符串切片
+		var result []string
+		for _, publisher := range publishers {
+			result = append(result, fmt.Sprintf("%+v", publisher))
+		}
+		return result, rowCount, colCount, siteNames
 	}
-	return nil
+	return nil, 0, 0, nil
 }
 
 func (dbmp *DBM) vertical_fragmentation(sql_s string, TableName string) []datatype.Book {
--- main.go
+++ main.go
@@ -22,9 +22,10 @@ func main() {
 
 	for {
 		//Input the SQL
-		// SQL_s := "select * from customer where id<300100"
+		SQL_s := "select * from customer"
+		//SQL_s := "select * from customer where id <= 310000"
 		// SQL_s := "select * from book where publisher_id<300050"
-		SQL_s := "select * from book,publisher where book.publisher_id = publisher.id and publisher.name= 'Twomorrows Publishing' and book.copies>6000"
+		// SQL_s := "select * from book,publisher where book.publisher_id = publisher.id and publisher.name= 'Twomorrows Publishing' and book.copies>6000"
 		dbm.Do(SQL_s)
 
 		break
